65f71045f7d4559ef855014d5fa2c52ee10c46ee
antony <antony.jose@innovaturelabs.com>
2023-10-16 12:20:02 +0530

    bus completed

--- source_code/client/bus_buddy_front_end/src/components/Updatebus.js
+++ source_code/client/bus_buddy_front_end/src/components/Updatebus.js
@@ -7,7 +7,8 @@ import Card from 'react-bootstrap/Card';
 import { BrowserRouter as Router, Route, Switch,Link, Routes } from 'react-router-dom';
 
 export default function Updatebus() {
-  const [id,setid]=useState("");
+  const [id,setid]=useState(0);
+  const [uid,setuid]=useState(1);
   const [bus_name,setbus_name]=useState("");
   const [plate_no,setplate_no]=useState("");
   const [status,setstatus]=useState("");
@@ -17,26 +18,31 @@ export default function Updatebus() {
     const navi = useNavigate();
 
 
-    function handleChange(e){
-      const value=e.target.value;
-      const name=e.target.name;
-      setInputs(prevState => ({
-           ...prevState,[name]:value
-      }))
-    }
+    // function handleChange(e){
+    //   const value=e.target.value;
+    //   const name=e.target.name;
+    //   setInputs(prevState => ({
+    //        ...prevState,[name]:value
+    //   }))
+    // }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         try {
+          const data = new FormData(e.currentTarget);
           // Send a POST request to add the task
-          axios.put("http://127.0.0.1:8000/updatebus/1/", {
+          axios.put(`http://127.0.0.1:8000/updatebus/${id}/`, 
+          {
             user:1,
-            bus_name: bus_name,
-            plate_no:plate_no,
-            status: status,
-            bus_type:bus_type,
-            bus_ac:bus_ac
-          });
+            bus_name:data.get("bus_name"),
+            plate_no:data.get("plate_no"),
+            status:data.get("status"),
+            bus_type:data.get("bus_type"),
+            bus_ac:data.get("bus_ac"),
+          }
+            
+            
+          );
           console.log("updated");
           // After successfully adding the task, navigate to the "Viewalltask" route
           // navi('/Viewalltask');
@@ -53,17 +59,19 @@ export default function Updatebus() {
         <Card.Text style={{display:'flex',}}>
         <form onSubmit={handleSubmit} >
          <label for="user">Bus ID:</label>
-         <input type="text" id="id" name="id" onChange={(e)=>{setid(e.target.value)}}/><br/><br/>
+         <input type="text" id="id" name="id"  onChange={(e)=>{setid(e.target.value)}}/><br/><br/>
+         <label for="user">User ID:</label>
+         <input type="text" id="id" name="id" value={1}/><br/><br/>
          <label for="bus_name">Bus name:</label>
-         <input type="text" id="bus_name" name="bus_name"  value={inputs.bus_name||""} onChange={handleChange}/><br/><br/>
+         <input type="text" id="bus_name" name="bus_name"  /><br/><br/>
          <label for="plate_no">Plate No:</label>
-         <input type="text" id="plate_no" name="plate_no" value={inputs.plate_no||""} onChange={handleChange}/><br/><br/>
+         <input type="text" id="plate_no" name="plate_no" /><br/><br/>
          <label for="status">Status:</label>
-         <input type="text" id="status" name="status" value={inputs.status||""} onChange={handleChange}/><br/><br/>
+         <input type="text" id="status" name="status" /><br/><br/>
          <label for="bus_type">Bus Type:</label>
-         <input type="text" id="bus_type" name="bus_type" value={inputs.bus_type||""} onChange={handleChange}/><br/><br/>
+         <input type="text" id="bus_type" name="bus_type"/><br/><br/>
          <label for="bus_ac">Bus A/C:</label>
-         <input type="text" id="bus_ac" name="bus_ac" value={inputs.bus_ac||""} onChange={handleChange}/><br/><br/>
+         <input type="text" id="bus_ac" name="bus_ac"/><br/><br/>
          <button type="submit" className="btn btn-success">Update</button>
         </form> 
         </Card.Text>
--- source_code/web/bus_buddy_back_end/bus_owner/views.py
+++ source_code/web/bus_buddy_back_end/bus_owner/views.py
@@ -9,7 +9,7 @@ from .serializers import buddyserializer
 logger = logging.getLogger(__name__)
 from django.core.exceptions import ObjectDoesNotExist 
 from rest_framework.exceptions import ValidationError
-from django.contrib.auth.models import User
+from account_manage.models import User
 
 # Create your views here.
 
@@ -49,10 +49,12 @@ class Updatebus(APIView):
     # permission_classes=(AllowAny,)
     def put(self, request,id):
         try:
+            print("g")
             serializer = buddyserializer(data=request.data)
             data=Bus.objects.get(id=id)
+            print("h")
             if serializer.is_valid(): 
-                data.user=1
+                data.user = User.objects.get(id=1)
                 data.bus_name=serializer.validated_data['bus_name']
                 data.plate_no=serializer.validated_data['plate_no']
                 data.status=serializer.validated_data['status']
@@ -60,6 +62,7 @@ class Updatebus(APIView):
                 data.bus_ac=serializer.validated_data['bus_ac']
                 data.save()
                 logger.info("updated")
+                print("i")
                 return Response("Updated",status=200)
             else:
                 return Response(serializer.errors, status=400)
