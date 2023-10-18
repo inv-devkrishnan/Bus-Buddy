e862d727052114b22dd3532d9eddb2886b23e471
Farsana <farsana.ka@innovaturelabs.com>
2023-10-18 12:53:13 +0530

    <feat: route addition>

---
+++ source_code/client/bus_buddy_front_end/src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
