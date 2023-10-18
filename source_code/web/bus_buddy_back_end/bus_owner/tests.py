7b00d8c6307e9ea4403ab003c8335c9dbed2c05e
Farsana <farsana.ka@innovaturelabs.com>
2023-10-18 12:46:44 +0530

    <chore: test.py has been completed>

--- source_code/web/bus_buddy_back_end/bus_owner/tests.py
+++ source_code/web/bus_buddy_back_end/bus_owner/tests.py
@@ -1,3 +1,149 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+valid_first_name = "Sakki"
+valid_last_name = "Sayya"
+valid_email = "SakkiSayya999@gmail.com"
+valid_phone = "9961006248"
+valid_company_name = "ABC Travels"
+valid_aadhaar = "147258369014"
+valid_msme = "UDYAN-654-745896"
+valid_extra_charges = 150.25
+
+
+class BaseTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.register_bus_owner = reverse("register-bus-owner")
+
+        self.register_data_owner = {
+            "first_name": "Priya",
+            "last_name": "Menoth",
+            "email": "priyaMenoth@gmail.com",
+            "phone": "9495130217",
+            "password": "7&hHi*7hs",
+            "company_name": "Priya Travels",
+            "aadhaar_no": "748159263578",
+            "msme_no": "UDYAN-451-784512",
+            "extra_charges": 455.50,
+        }
+
+        self.valid_data = {
+            "first_name": valid_first_name,
+            "last_name": valid_last_name,
+            "email": valid_email,
+            "phone": valid_phone,
+            "company_name": valid_company_name,
+            "aadhaar_no": valid_aadhaar,
+            "msme_no": valid_msme,
+            "extra_charges": valid_extra_charges,
+        }
+
+        self.invalid_names = {
+            "first_name": "56",
+            "last_name": "789",
+            "email": valid_email,
+            "phone": valid_phone,
+            "company_name": valid_company_name,
+            "aadhaar_no": valid_aadhaar,
+            "msme_no": valid_msme,
+            "extra_charges": valid_extra_charges,
+        }
+
+        self.invalid_email = {
+            "first_name": valid_first_name,
+            "last_name": valid_last_name,
+            "email": "SakkiSayya",
+            "phone": valid_phone,
+            "company_name": valid_company_name,
+            "aadhaar_no": valid_aadhaar,
+            "msme_no": valid_msme,
+            "extra_charges": valid_extra_charges,
+        }
+
+        self.invalid_phone_length = {
+            "first_name": valid_first_name,
+            "last_name": valid_last_name,
+            "email": valid_email,
+            "phone": "9876",
+            "company_name": valid_company_name,
+            "aadhaar_no": valid_aadhaar,
+            "msme_no": valid_msme,
+            "extra_charges": valid_extra_charges,
+        }
+
+        self.invalid_phone_alphabet = {
+            "first_name": valid_first_name,
+            "last_name": valid_last_name,
+            "email": valid_email,
+            "phone": "987654321o",
+            "company_name": valid_company_name,
+            "aadhaar_no": valid_aadhaar,
+            "msme_no": valid_msme,
+            "extra_charges": valid_extra_charges,
+        }
+
+        return super().setUp()
+
+
+class RegisterUserTest(BaseTest):
+    def test_can_register_user(self):
+        self.client.post(
+            self.register_bus_owner, self.register_data_owner, format="json"
+        )
+        response = self.client.put(
+            reverse("update-profile-owner", kwargs={"id":1}), self.valid_data, format="json"
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_cant_register_user_with_invalid_names(self):
+        self.client.post(
+            self.register_bus_owner, self.register_data_owner, format="json"
+        )
+        response = self.client.put(
+            reverse("update-profile-owner", kwargs={"id":4}),
+            self.invalid_names,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_cant_register_user_with_invalid_email(self):
+        self.client.post(
+            self.register_bus_owner, self.register_data_owner, format="json"
+        )
+        response = self.client.put(
+            reverse("update-profile-owner", kwargs={"id":3}),
+            self.invalid_email,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_cant_register_user_with_invalid_phone_length(self):
+        self.client.post(
+            self.register_bus_owner, self.register_data_owner, format="json"
+        )
+        response = self.client.put(
+            reverse("update-profile-owner", kwargs={"id":6}),
+            self.invalid_phone_alphabet,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_cant_register_user_with_invalid_phone_alphabet(self):
+        self.client.post(
+            self.register_bus_owner, self.register_data_owner, format="json"
+        )
+        response = self.client.put(
+            reverse("update-profile-owner", kwargs={"id":5}),
+            self.invalid_phone_length,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 400)
