a5085dce6f27bc7d0f923cb2462b18a59e4f395d
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2023-10-18 13:04:03 +0530

    feat:getlocationdata

--- source_code/web/bus_buddy_back_end/bus_buddy_back_end/urls.py
+++ source_code/web/bus_buddy_back_end/bus_buddy_back_end/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import GetLocationData
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("get-location-data/",GetLocationData.as_view())
 ]
