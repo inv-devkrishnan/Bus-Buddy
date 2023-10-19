a954e7affe197f6defbe1258991754b6ecc8fafa
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2023-10-19 15:40:20 +0530

    feat: add Route ui

--- source_code/client/bus_buddy_front_end/src/Components/AddRouteCard.jsx
+++ source_code/client/bus_buddy_front_end/src/Components/AddRouteCard.jsx
@@ -3,107 +3,235 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import Table from "react-bootstrap/Table";
 
 import RouteImage from "../Assets/route.jpg";
 import axios from "axios";
+import AddRouteLocation from "./AddRouteLocation";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function AddRouteCard() {
   const [locations, setLocations] = useState([]);
+  const [sequenceId, setSequenceId] = useState(1);
+  const [stopLocations, addStopLocation] = useState([]);
+  const [locationAdded, setlocationAdded] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [show, setShow] = useState(false);
+  const [validated, setValidated] = useState(false);
+  const handleClose = () => {
+    setShow(false);
+    setlocationAdded(false);
+  };
+  const handleShow = () => setShow(true);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      addRoute();
+    }
+
+    setValidated(true);
+  };
 
   useEffect(() => {
     axios
       .get("http://127.0.0.1:8000/get-location-data/")
       .then((res) => {
         setLocations(res.data);
-        console.log(locations);
-        console.log(locations[0]["location_name"]);
       })
       .catch((err) => {});
   }, []);
 
+  const appendStopLocation = (newStopLocation) => {
+    console.log(newStopLocation);
+    addStopLocation((StopLocation) => [...StopLocation, newStopLocation]);
+  };
+
+  const addRoute = () => {
+    if (stopLocations.length >= 2) {
+      const routeData=
+      {
+        start_point :  stopLocations[0].location_id,
+        end_point : stopLocations[stopLocations.length-1].location_id,
+        via : document.getElementById("via").value,
+        distance : document.getElementById("distance").value,
+        duration : document.getElementById("duration").value,
+        travel_fare : document.getElementById("travel_fare").value,
+        status : 0,
+        location : stopLocations
+      }
+      console.log(routeData)
+      setErrorMessage("");
+    } else {
+      setErrorMessage("Submission Failed: Atleast add 2 locations !");
+    }
+  };
+
+  const getLocationName = (locationId) => {
+    let locationName;
+    locations.forEach((element) => {
+      if (element.id === locationId) {
+        locationName = element.location_name;
+      }
+    });
+    return locationName;
+  };
+
   return (
-    <Card style={{ width: "50rem" }}>
-      <Card.Img variant="top" src={RouteImage} />
-      <Card.Body>
-        <Card.Title>Enter Route Details</Card.Title>
-        <Card.Text>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Starting location</Form.Label>
-              {locations && (
-                <Form.Select
-                  aria-label="Default select example"
-                  name="starting"
-                >
-                  {locations.map((location) => (
-                    <>
-                      <option key={location.id} value={location.id}>
-                        {location.location_name}
-                      </option>
-                    </>
-                  ))}
-                </Form.Select>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Ending location</Form.Label>
-              {locations && (
-                <Form.Select aria-label="Default select example" name="ending">
-                  {locations.map((location) => (
-                    <>
-                      <option key={location.id} value={location.id}>
-                        {location.location_name}
-                      </option>
-                    </>
-                  ))}
-                </Form.Select>
-              )}
-            </Form.Group>
+    <Container className="w-50">
+      <Row>
+        <Col>
+          <Card className="mb-5 mt-5">
+            <div className="d-flex align-items-center">
+              <Card.Img variant="top w-25" src={RouteImage} />
+              <h1>Add Route</h1>
+            </div>
+            <Card.Body>
+              <Card.Title className="mb-3">Enter Route Details</Card.Title>
+              <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Group className="mt-3 mb-3">
+                  <Form.Label>Add Locations</Form.Label>
+                  <Form.Text className="d-flex">
+                    Add all the locations in the acending order of arival during
+                    this route.
+                  </Form.Text>
+                  <Button
+                    className="d-block mt-2 mb-2"
+                    variant="success"
+                    onClick={handleShow}
+                  >
+                    Add Location
+                  </Button>
+                  {stopLocations.length > 0 && (
+                    <div>
+                      <Table className="w-75">
+                        <thead>
+                          <tr>
+                            <th>S.no</th>
+                            <th>Location Name</th>
+                            <th>Location Stops</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {stopLocations.map((stopLocation) => (
+                            <tr key={stopLocation.seq_id}>
+                              <td>{stopLocation.seq_id}</td>
+                              <td>
+                                {getLocationName(stopLocation.location_id)}
+                              </td>
+                              <td>
+                                <ul>
+                                  {stopLocation.pick_and_drop.map((stops) => (
+                                    <li key={stops.bus_stop}>
+                                      {stops.bus_stop}
+                                    </li>
+                                  ))}
+                                </ul>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </Table>
+                      <Button
+                        variant="danger"
+                        onClick={() => {
+                          addStopLocation([]);
+                          setSequenceId(1);
+                        }}
+                      >
+                        Reset
+                      </Button>
+                    </div>
+                  )}
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Via Location</Form.Label>
+                  <Form.Control
+                    id ="via"
+                    type="text"
+                    placeholder="Enter the location"
+                    pattern="^[a-zA-Z]+$"
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide valid Via location
+                  </Form.Control.Feedback>
+                  <Form.Text muted>
+                    Via is the location that helps the travellers to identify in
+                    which way the bus travels
+                  </Form.Text>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Via</Form.Label>
-              <Form.Control type="text" placeholder="Enter the location" />
-              <Form.Text muted>
-                Via is the location that helps the travellers to identify in
-                which way the bus travells
-              </Form.Text>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Duration</Form.Label>
+                  <Form.Control
+                    id="duration"
+                    type="time"
+                    placeholder="Enter the total time"
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide valid Duration
+                  </Form.Control.Feedback>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Duration</Form.Label>
-              <Form.Control type="time" placeholder="Enter the total time" />
-            </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Distance</Form.Label>
+                  <InputGroup>
+                    <Form.Control
+                      id="distance"
+                      type="number"
+                      step={0.01}
+                      placeholder="Enter the total distance"
+                      required
+                    />
+                    <InputGroup.Text>km</InputGroup.Text>
+                    <Form.Control.Feedback type="invalid">
+                    Please provide valid Distance
+                  </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Distance</Form.Label>
-              <InputGroup>
-                <Form.Control
-                  type="number"
-                  step={0.5}
-                  placeholder="Enter the total distance"
-                />
-                <InputGroup.Text>km</InputGroup.Text>
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Travel fare</Form.Label>
-              <InputGroup>
-                <InputGroup.Text>&#8377;</InputGroup.Text>
-                <Form.Control
-                  type="number"
-                  placeholder="Enter the travel fare"
-                  min={0}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+                <Form.Group className="mb-3">
+                  <Form.Label>Travel fare</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text>&#8377;</InputGroup.Text>
+                    <Form.Control
+                      id="travel_fare"
+                      type="number"
+                      placeholder="Enter the travel fare"
+                      min={0}
+                      required
+                      step="0.01"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                    Please provide valid travel fare
+                  </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+                <label className="text-danger d-block">{errorMessage}</label>
+                <Button variant="primary mt-2" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+            <AddRouteLocation
+              show={show}
+              handleClose={handleClose}
+              locations={locations}
+              appendStopLocation={appendStopLocation}
+              sequenceId={sequenceId}
+              setSequenceId={setSequenceId}
+              locationAdded={locationAdded}
+              setlocationAdded={setlocationAdded}
+              stopLocations={stopLocations}
+            ></AddRouteLocation>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
---
+++ source_code/client/bus_buddy_front_end/src/Components/AddRouteLocation.jsx
@@ -0,0 +1,279 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { Form } from "react-bootstrap";
+import { useState } from "react";
+function AddRouteLocation(props) {
+  const [locationValue, setLocationValue] = useState(1);
+  const [arrivalTime, setArrivalTime] = useState("");
+  const [arrivalDate, setArrivalDate] = useState("");
+  const [departureTime, setDepartureTime] = useState("");
+  const [departureDate, setDepartureDate] = useState("");
+  const [locationFormValidated, setlocationFormValidated] = useState(false);
+  const [location, setLocation] = useState("");
+
+  const [stopName, setStopName] = useState("");
+  const [stopArrivalTime, setStopArrivalTime] = useState("");
+  const [landmark, setLandmark] = useState("");
+  const [stopFormValidated, setStopFormValidated] = useState(false);
+  const [stopsArray, setStopsArray] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const checkLocationAlreadyExists = (currentLocationId) => {
+    let status = false;
+    props.stopLocations.forEach((element) => {
+      if (element.location_id === currentLocationId) {
+        status = true;
+      }
+    });
+    return status;
+  };
+  const locationHandleSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    if (form.checkValidity() === false) {
+      // to handle submissions of locations;
+
+      event.stopPropagation();
+      setlocationFormValidated(true);
+    } else if (checkLocationAlreadyExists(locationValue)) {
+      setErrorMessage("This location is already added");
+    }else if(arrivalDate>departureDate)
+    {
+      setErrorMessage("Arrival date offset can't be past depature date offset");
+    }
+     else if (arrivalTime > departureTime && arrivalDate === departureDate)
+    {
+      setErrorMessage("Arrival time can't be past depature time")
+    }
+     else {
+      const locationStop = {
+        seq_id: props.sequenceId,
+        location_id: locationValue,
+        arrival_time: arrivalTime,
+        arrival_date: arrivalDate,
+        departure_time: departureTime,
+        departure_date: departureDate,
+      };
+
+      setLocation(locationStop);
+      setLocationValue(1);
+      setArrivalTime("");
+      setArrivalDate("");
+      setDepartureTime("");
+      setDepartureDate("");
+      setlocationFormValidated(false);
+      setErrorMessage("");
+      props.setlocationAdded(true);
+    }
+  };
+
+  const stopHandleSubmit = (event) => {
+    // to handle submissions of stops;
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setStopFormValidated(true);
+    } else {
+      event.preventDefault();
+      const newStop = {
+        bus_stop: stopName,
+        arrival_time: stopArrivalTime,
+        landmark: landmark,
+        status: 0,
+      };
+      setStopsArray((stop) => [...stop, newStop]);
+      setStopName("");
+      setStopArrivalTime("");
+      setLandmark("");
+      setErrorMessage("");
+      console.log(props.sequenceId);
+      setStopFormValidated(false);
+    }
+  };
+
+  const saveDetails = () => {
+    if (stopsArray.length > 0) {
+      let currentStopLocation = location;
+      currentStopLocation["pick_and_drop"] = stopsArray;
+      setStopsArray([]); // clears stops array for next location
+      props.appendStopLocation(currentStopLocation);
+      props.setSequenceId(props.sequenceId + 1);
+      props.handleClose();
+    } else {
+      setErrorMessage("At least add one stop");
+    }
+  };
+  return (
+    <Modal show={props.show} onHide={props.handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          {props.locationAdded ? "Add Stops" : "Add Location"}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {props.locationAdded ? (
+          <Form
+            noValidate
+            validated={stopFormValidated}
+            onSubmit={stopHandleSubmit}
+          >
+            <Form.Group className="mb-3">
+              <Form.Label>Stop Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Stop Name"
+                value={stopName}
+                onChange={(e) => {
+                  setStopName(e.target.value);
+                }}
+                pattern="^[a-zA-Z0-9 ]+$"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide valid Stop Name
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label> Stop Arrival Time</Form.Label>
+              <Form.Control
+                type="time"
+                placeholder="Time"
+                value={stopArrivalTime}
+                onChange={(e) => {
+                  setStopArrivalTime(e.target.value);
+                }}
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid time.
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Landmark</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Landmark"
+                value={landmark}
+                onChange={(e) => {
+                  setLandmark(e.target.value);
+                }}
+                pattern="^[a-zA-Z0-9 ]+$"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a landmark
+              </Form.Control.Feedback>
+            </Form.Group>
+            <label className="text-danger d-block ms-2 me-2">
+              {errorMessage}
+            </label>
+            <Button type="submit" className="ms-2 me-2">
+              Add Stop
+            </Button>
+            <Button
+              variant="success"
+              onClick={saveDetails}
+              className="ms-2 me-2"
+            >
+              Save
+            </Button>
+          </Form>
+        ) : (
+          <Form
+            noValidate
+            validated={locationFormValidated}
+            onSubmit={locationHandleSubmit}
+          >
+            <Form.Group className="mb-3">
+              <Form.Label>Location</Form.Label>
+              {props.locations && (
+                <Form.Select
+                  aria-label="Default select example"
+                  name="ending"
+                  value={locationValue}
+                  onChange={(e) => {
+                    setLocationValue(Number(e.target.value));
+                  }}
+                >
+                  {props.locations.map((location) => (
+                    <>
+                      <option key={location.id} value={location.id}>
+                        {location.location_name}
+                      </option>
+                    </>
+                  ))}
+                </Form.Select>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Arrival Time</Form.Label>
+              <Form.Control
+                type="time"
+                placeholder="Enter time"
+                value={arrivalTime}
+                onChange={(e) => {
+                  setArrivalTime(e.target.value);
+                }}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Arrival Date offset</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter number of days"
+                min={0}
+                max={10}
+                value={arrivalDate}
+                onChange={(e) => {
+                  setArrivalDate(e.target.value);
+                }}
+                required
+              />
+              <Form.Text className="text-muted">
+                Number of days which is required to reach this location from the start date of the trip
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Departure Time</Form.Label>
+              <Form.Control
+                type="time"
+                placeholder="Enter time"
+                value={departureTime}
+                onChange={(e) => {
+                  setDepartureTime(e.target.value);
+                }}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Departure Date offset</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter number of days"
+                min={0}
+                max={10}
+                value={departureDate}
+                onChange={(e) => {
+                  setDepartureDate(e.target.value);
+                }}
+                required
+              />
+              <Form.Text className="text-muted">
+              Number of days after which we leave this location from the start date of the trip
+              </Form.Text>
+            </Form.Group>
+            <label className="text-danger d-block ms-2 me-2">
+              {errorMessage}
+            </label>
+            <Button variant="primary mt-2" type="submit">
+              Add Location
+            </Button>
+          </Form>
+        )}
+      </Modal.Body>
+    </Modal>
+  );
+}
+export default AddRouteLocation;
