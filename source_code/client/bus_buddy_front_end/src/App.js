313d452536bea1e2072c74832081943d30378f76
antony <antony.jose@innovaturelabs.com>
2023-10-24 12:44:45 +0530

    name

--- source_code/client/bus_buddy_front_end/src/App.js
+++ source_code/client/bus_buddy_front_end/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BusHome from "./components/Bus_ui/BusHome";
-import Addbus from './components/Bus_ui/Addbus';
-import Deletebus from './components/Bus_ui/Deletebus';
-import Updatebus from './components/Bus_ui/Updatebus';
-import Viewbus from './components/Bus_ui/Viewbus';
-import Addamenities from './components/Bus_ui/Addamenities';
-import Updateamenities from './components/Bus_ui/Updateamenities'
+import BusHome from "./components/Bus_Ui/BusHome";
+import AddBus from './components/Bus_Ui/AddBus';
+import DeleteBus from './components/Bus_Ui/DeleteBus';
+import UpdateBus from './components/Bus_Ui/UpdateBus';
+import ViewBus from './components/Bus_Ui/ViewBus';
+import AddAmenities from './components/Bus_Ui/AddAmenities';
+import UpdateAmenities from './components/Bus_Ui/UpdateAmenities'
 import 'bootstrap/dist/css/bootstrap.css';
 import Addroutes from './components/route_ui/Addroutes';
 import Deleteroutes from './components/route_ui/Deleteroutes';
@@ -20,12 +20,12 @@ function App() {
           <React.Fragment>
             <Routes>
               <Route path='/' element={<BusHome/>}/>
-              <Route path='/Addbus' element={<Addbus/>}/>
-              <Route path='/Deletebus' element={<Deletebus/>}/>
-              <Route path='/Updatebus' element={<Updatebus/>}/>
-              <Route path='/Updateamenities' element={<Updateamenities/>}/>
-              <Route path='/Viewbus' element={<Viewbus/>}/>
-              <Route path='/Addamenities' element={<Addamenities/>}/>
+              <Route path='/Addbus' element={<AddBus/>}/>
+              <Route path='/Deletebus' element={<DeleteBus/>}/>
+              <Route path='/Updatebus' element={<UpdateBus/>}/>
+              <Route path='/Viewbus' element={<ViewBus/>}/>
+              <Route path='/Addamenities' element={<AddAmenities/>}/>
+              <Route path='/Updateamenities' element={<UpdateAmenities/>}/>
               <Route path='/Addroutes' element={<Addroutes/>}/>
               <Route path='/Deleteroutes' element={<Deleteroutes/>}/>
               <Route path='/Updateroutes' element={<Updateroutes/>}/>
