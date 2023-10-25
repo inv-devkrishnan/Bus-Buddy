f1df10cec3c2552ac0cf4c105107416edadabaac
Farsana <farsana.ka@innovaturelabs.com>
2023-10-25 18:56:17 +0530

    <refactor: update profile of user and owner>

--- source_code/web/bus_buddy_back_end/bus_owner/views.py
+++ source_code/web/bus_buddy_back_end/bus_owner/views.py
@@ -2,75 +2,12 @@ from django.shortcuts import render
 from django.db.models import Q  # to check not equal in filter
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import UpdateAPIView
 from rest_framework.permissions import AllowAny
 from account_manage.models import User
 from bus_owner.serializer import OwnerModelSerializer as OMS
-from bus_owner.serializer import OwnerUpdateSerializer as AllField
-from bus_owner.serializer import OwnerUpdateExceptEmailSerializer as ExceptEmail
-from bus_owner.serializer import OwnerUpdateExceptPhoneSerializer as ExceptPhone
-from bus_owner.serializer import OwnerUpdateOnlyNamesSerializer as OnlyName
 
 
-# commonly used messages
-update_message = {"message": "updated succesffully"}
-
-
-def update_only_name(request, user_id):
-    current_data = User.objects.get(id=user_id)
-    serialized_data = OnlyName(data=request.data)
-    if serialized_data.is_valid():
-        current_data.first_name = serialized_data.validated_data["first_name"]
-        current_data.last_name = serialized_data.validated_data["last_name"]
-        current_data.company_name = serialized_data.validated_data["company_name"]
-        current_data.save()
-        return True
-    else:
-        return serialized_data._errors
-
-
-def update_except_email(request, user_id):
-    current_data = User.objects.get(id=user_id)
-    serialized_data = ExceptEmail(data=request.data)
-    if serialized_data.is_valid():
-        current_data.first_name = serialized_data.validated_data["first_name"]
-        current_data.last_name = serialized_data.validated_data["last_name"]
-        current_data.company_name = serialized_data.validated_data["company_name"]
-        current_data.phone = serialized_data.validated_data["phone"]
-        current_data.save()
-        return True
-    else:
-        return serialized_data._errors
-
-
-def update_except_phone(request, user_id):
-    current_data = User.objects.get(id=user_id)
-    serialized_data = ExceptPhone(data=request.data)
-    if serialized_data.is_valid():
-        current_data.first_name = serialized_data.validated_data["first_name"]
-        current_data.last_name = serialized_data.validated_data["last_name"]
-        current_data.company_name = serialized_data.validated_data["company_name"]
-        current_data.email = serialized_data.validated_data["email"]
-        current_data.save()
-        return True
-    else:
-        return serialized_data._errors
-    
-def update_all(request, user_id):
-    current_data = User.objects.get(id=user_id)
-    serialized_data = AllField(data=request.data)
-    if serialized_data.is_valid():
-        current_data.first_name = serialized_data.validated_data["first_name"]
-        current_data.last_name = serialized_data.validated_data["last_name"]
-        current_data.company_name = serialized_data.validated_data["company_name"]
-        current_data.email = serialized_data.validated_data["email"]
-        current_data.phone = serialized_data.validated_data["phone"]
-        current_data.save()
-        return True
-    else:
-        return serialized_data._errors
-    
-    
-    
 class RegisterBusOwner(APIView):
     permission_classes = (AllowAny,)
     
@@ -85,39 +22,21 @@ class RegisterBusOwner(APIView):
             return Response(serialized_data._errors,status=400)
 
 
-class UpdateBusOwner(APIView):
+class UpdateBusOwner(UpdateAPIView):
     permission_classes = (AllowAny,)
-
-    def get(self, request, id):
+    
+    def update(self, request, id):
         try:
-            user = User.objects.get(id=id)
-        except User.DoesNotExist:
-            return Response(status=404)
-        
-        serialized_data=AllField(user)
-        return Response(serialized_data.data)
+            instance = User.objects.get(id=id)
+            current_data = request.data
+            serializer = OMS(instance, data=current_data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response({"message": "updated succesffully"}, status=200)
+        except TypeError:
+            return Response(serializer.errors,status=400)
 
     def put(self, request, id):
-        user_id = id
-        entered_email = request.data.get("email")
-        entered_phone = request.data.get("phone")
+        return self.update(request, id)
 
-        if User.objects.all().filter(
-            email__contains=entered_email, phone=entered_phone # old email and phone
-        ):
-            response=update_only_name(request, user_id)
-        elif User.objects.all().filter(
-            ~Q(phone=entered_phone), email__contains=entered_email # old email only
-        ):
-            response=update_except_email(request, user_id)
-        elif User.objects.all().filter(
-            ~Q(email__contains=entered_email), phone=entered_phone # old phone only
-        ):
-            response=update_except_phone(request, user_id)
-        else: # every data is different from old
-            response=update_all(request, user_id)
-            
-        if response==True:
-            return Response(update_message,status=200)
-        else:
-            return Response(response,status=400) 
+ 
--- source_code/web/bus_buddy_back_end/normal_user/views.py
+++ source_code/web/bus_buddy_back_end/normal_user/views.py
@@ -6,66 +6,6 @@ from rest_framework.generics import UpdateAPIView
 from rest_framework.permissions import AllowAny
 from account_manage.models import User
 from normal_user.serializer import UserModelSerializer as UMS
-from normal_user.serializer import UserUpdateSerializer as AllField
-from normal_user.serializer import UserUpdateOnlyNameSerializer as OnlyName
-from normal_user.serializer import UserUpdateExceptEmailSerializer as ExceptEmail
-from normal_user.serializer import UserUpdateExceptPhoneSerializer as ExceptPhone
-
-# commonly used messages
-update_message = {"message": "updated succesffully"}
-
-
-def update_only_name(request, user_id):
-    current_data = User.objects.get(id=user_id)
-    serialized_data = OnlyName(data=request.data)
-    if serialized_data.is_valid():
-        current_data.first_name = serialized_data.validated_data["first_name"]
-        current_data.last_name = serialized_data.validated_data["last_name"]
-        current_data.save()
-        return True
-    else:
-        return serialized_data._errors
-
-
-def update_except_email(request, user_id):
-    current_data = User.objects.get(id=user_id)
-    serialized_data = ExceptEmail(data=request.data)
-    if serialized_data.is_valid():
-        current_data.first_name = serialized_data.validated_data["first_name"]
-        current_data.last_name = serialized_data.validated_data["last_name"]
-        current_data.phone = serialized_data.validated_data["phone"]
-        current_data.save()
-        return True
-    else:
-        return serialized_data._errors
-
-
-def update_except_phone(request, user_id):
-    current_data = User.objects.get(id=user_id)
-    serialized_data = ExceptPhone(data=request.data)
-    if serialized_data.is_valid():
-        current_data.first_name = serialized_data.validated_data["first_name"]
-        current_data.last_name = serialized_data.validated_data["last_name"]
-        current_data.email = serialized_data.validated_data["email"]
-        current_data.save()
-        return True
-    else:
-        return serialized_data._errors
-
-
-def update_all(request, user_id):
-    current_data = User.objects.get(id=user_id)
-    serialized_data = AllField(data=request.data)
-    if serialized_data.is_valid():
-        current_data.first_name = serialized_data.validated_data["first_name"]
-        current_data.last_name = serialized_data.validated_data["last_name"]
-        current_data.email = serialized_data.validated_data["email"]
-        current_data.phone = serialized_data.validated_data["phone"]
-        current_data.save()
-        return True
-    else:
-        return serialized_data._errors
-
 
 class RegisterUser(APIView):
     permission_classes = (AllowAny,)
@@ -79,43 +19,7 @@ class RegisterUser(APIView):
             return Response(serialized_data._errors, status=400)
 
 
-class UpdateProfile(APIView):
-    permission_classes = (AllowAny,)
-
-    def get(self, request, id):
-        try:
-            user = User.objects.get(id=id)
-        except User.DoesNotExist:
-            return Response(status=404)
-
-        serialized_data = AllField(user)
-        return Response(serialized_data.data)
-
-    def put(self, request, id):
-        entered_email = request.data.get("email")
-        entered_phone = request.data.get("phone")
-        if User.objects.all().filter(
-            email__icontains=entered_email, phone=entered_phone  # old email and phone
-        ):
-            response = update_only_name(request, id)
-        elif User.objects.all().filter(
-            ~Q(phone=entered_phone), email__icontains=entered_email  # old email
-        ):
-            response = update_except_email(request, id)
-        elif User.objects.all().filter(
-            ~Q(email__icontains=entered_email), phone=entered_phone  # old phone
-        ):
-            response = update_except_phone(request, id)
-        else:  # every data is different from old
-            response = update_all(request, id)
-
-        if response == True:
-            return Response(update_message, status=200)
-        else:
-            return Response(response, status=400)
-
-
-class UpdateUser(UpdateAPIView):
+class UpdateProfile(UpdateAPIView):
     permission_classes = (AllowAny,)
 
     def update(self, request, id):
@@ -125,8 +29,8 @@ class UpdateUser(UpdateAPIView):
             serializer = UMS(instance, data=current_data, partial=True)
             serializer.is_valid(raise_exception=True)
             self.perform_update(serializer)
-            return Response(serializer.data, status=200)
-        except User.DoesNotExist:
+            return Response({"message": "updated succesffully"}, status=200)
+        except TypeError:
             return Response(serializer.errors,status=400)
 
     def put(self, request, id):
