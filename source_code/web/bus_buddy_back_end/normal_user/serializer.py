8a5962ab938a38ac5254f0b90fa66686faad017b
Farsana <farsana.ka@innovaturelabs.com>
2023-10-27 09:03:11 +0530

    chore: password encryption

--- source_code/web/bus_buddy_back_end/normal_user/serializer.py
+++ source_code/web/bus_buddy_back_end/normal_user/serializer.py
@@ -66,6 +66,10 @@ class UserModelSerializer(serializers.ModelSerializer):
         ],
     )
     
+    # password encryption
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+    
 
 
 
