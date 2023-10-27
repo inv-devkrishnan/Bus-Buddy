d74b7fbb34f18c5730e065d15e3ab0d7b4e8490e
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2023-10-27 18:21:22 +0530

    chore: added testcases

--- source_code/web/bus_buddy_back_end/adminstrator/tests.py
+++ source_code/web/bus_buddy_back_end/adminstrator/tests.py
@@ -15,6 +15,9 @@ class BaseTest(TestCase):
         self.user = User.objects.create_user(
             email=self.email, password="12345678", account_provider=0, role=1
         )
+        self.user = User.objects.create_user(
+            email="dev@gmail.com", password="12345678", account_provider=0, role=1
+        )
         self.client.force_authenticate(self.user)
 
         # url
@@ -27,6 +30,36 @@ class BaseTest(TestCase):
             "email": "desk@gmail.com",
             "phone": "9037760634",
         }
+        self.valid_update_data_existing_email = {
+            "first_name": "Devkrishnan",
+            "last_name": "V D",
+            "email": "dev@gmail.com",
+            "phone": "9037760634",
+        }
+        self.update_data_invalid_first_name = {
+            "first_name": "Devkrish3an",
+            "last_name": "V D",
+            "email": "derk@gmail.com",
+            "phone": "9037760634",
+        }
+        self.update_data_invalid_last_name = {
+            "first_name": "Devkrishnan",
+            "last_name": "V3$",
+            "email": "desdk@gmail.com",
+            "phone": "9037760634",
+        }
+        self.update_data_invalid_phone_number = {
+            "first_name": "Devkrishnan",
+            "last_name": "V D",
+            "email": "desdk@gmail.com",
+            "phone": "903fsd7760634",
+        }
+        self.update_data_invalid_email = {
+            "first_name": "Devkrishnan",
+            "last_name": "V3$",
+            "email": "ddsdl",
+            "phone": "903fsd7760634",
+        }
 
 
 class UpdateAdminProfile(BaseTest):
@@ -36,4 +69,14 @@ class UpdateAdminProfile(BaseTest):
             data=self.valid_update_data,
             format="json",
         )
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)   
+    
+    def test_02_cannot_update_with_existing_email(self):
+        response = self.client.put(
+            self.admin_profile_update_url,
+            data=self.valid_update_data_existing_email,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 400)  
+        
+             
