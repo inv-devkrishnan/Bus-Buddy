2cc36513316085208b01a88dbc65dd3cb7b7c956
Farsana <farsana.ka@innovaturelabs.com>
2023-11-07 12:09:05 +0530

    chore: test.py initialized

--- source_code/web/bus_buddy_back_end/bus_owner/tests.py
+++ source_code/web/bus_buddy_back_end/bus_owner/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class BaseTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.add_seat_detail = reverse("add-seat-detail")
+
+        self.valid_all_values_seat_details = {
+            "bus": 1,
+            "seat_ui_order": 11,
+            "seat_number": "1",
+            "seat_type": 0,
+            "deck": 0,
+            "seat_cost": 200,
+        }
--- source_code/web/bus_buddy_back_end/normal_user/urls.py
+++ source_code/web/bus_buddy_back_end/normal_user/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("view-seats/", views.ViewSeats.as_view()),
+    path("view-seats/", views.ViewSeats.as_view(),name="add-seat-detail"),
 ]
