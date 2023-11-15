040af030b09f5e186fbc07240627322e7d67917a
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2023-11-15 15:00:03 +0530

    chore: added documentations ,improved code

--- source_code/client/bus_buddy_front_end/src/components/common/navbar/BusNavBar.jsx
+++ source_code/client/bus_buddy_front_end/src/components/common/navbar/BusNavBar.jsx
@@ -1,27 +1,25 @@
 import Container from "react-bootstrap/Container";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Navbar from "react-bootstrap/Navbar";
-import { PersonCircle } from "react-bootstrap-icons";
+import { PersonCircle,BusFrontFill } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useLogout } from "../../../utils/hooks/useLogout";
 function BusNavBar() {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // to navigate to different pages
   const logout = useLogout();
-  const [user, setUser] = useState({});
-
-  const navigateTo = (path) => {
-    navigate(path);
-  };
-
+  const [user, setUser] = useState({}); // to store current logged user details
   const getUserInfo = () => {
+    // function to get current user info from localstorage
     let user_name = localStorage.getItem("user_name");
     let user_role = localStorage.getItem("user_role");
     let user = {};
     if (user_name && user_role) {
+      // if user is logged in store name and role to user object
       user["name"] = user_name;
       user["role"] = user_role;
     } else {
+      // else store user name as Guest and role as -1
       user["name"] = "Guest";
       user["role"] = "-1";
     }
@@ -30,6 +28,7 @@ function BusNavBar() {
   };
 
   const getProfile = (role) => {
+    // different types of users have different dashboards the bellow function navigates to dashboards based on given role
     switch (role) {
       case "1":
         navigate("/admin-dashboard");
@@ -51,30 +50,39 @@ function BusNavBar() {
   return (
     <Navbar expand="lg" bg="primary" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand className="fw-bold">Bus Buddy</Navbar.Brand>
+        <Navbar.Brand
+          className="fw-bold"
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <BusFrontFill className="me-2"></BusFrontFill>
+          Bus Buddy
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <div className="ms-auto d-flex align-items-center">
             <PersonCircle className="me-3" color="white"></PersonCircle>
             {user.name === "Guest" ? (
-              <NavDropdown title="Hello Guest" className="text-light fw-bold">
+              <NavDropdown title="Hello Guest" className="text-light fw-bold" data-bs-theme="light">
                 <NavDropdown.Item
                   onClick={() => {
-                    navigateTo("/login");
+                    navigate("/login");
                   }}
                 >
                   Login
                 </NavDropdown.Item>
                 <NavDropdown.Item
                   onClick={() => {
-                    navigateTo("/register-user");
+                    navigate("/register-user");
                   }}
                 >
                   Register as user
                 </NavDropdown.Item>
                 <NavDropdown.Item
                   onClick={() => {
-                    navigateTo("/register-owner");
+                    navigate("/register-owner");
                   }}
                 >
                   Register as bus owner
@@ -82,8 +90,9 @@ function BusNavBar() {
               </NavDropdown>
             ) : (
               <NavDropdown
-                title={"Hello, " + user.name}
+                title={"Hello " + user.name}
                 className="text-light fw-bold"
+                data-bs-theme="light"
               >
                 <NavDropdown.Item
                   onClick={() => {
--- source_code/client/bus_buddy_front_end/src/components/common/navbar/NavBarLayout.jsx
+++ source_code/client/bus_buddy_front_end/src/components/common/navbar/NavBarLayout.jsx
@@ -2,8 +2,9 @@ import { Outlet } from "react-router-dom";
 import BusNavBar from "./BusNavBar";
 
 export const NavbarLayout = () => (
+  // function to display navbar and its children
   <div>
-    <BusNavBar />
+    <BusNavBar/>
     <Outlet/>
   </div>
 );
