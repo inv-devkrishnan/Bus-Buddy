70f6c654416fcc7e13a8a9269ef30c278efb5a4f
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2023-11-15 12:12:35 +0530

    chore: integrated navbar to application

--- source_code/client/bus_buddy_front_end/src/components/common/navbar/BusNavBar.jsx
+++ source_code/client/bus_buddy_front_end/src/components/common/navbar/BusNavBar.jsx
@@ -3,12 +3,51 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Navbar from "react-bootstrap/Navbar";
 import { PersonCircle } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLogout } from "../../../utils/hooks/useLogout";
 function BusNavBar() {
   const navigate = useNavigate();
+  const logout = useLogout();
+  const [user, setUser] = useState({});
 
   const navigateTo = (path) => {
     navigate(path);
   };
+
+  const getUserInfo = () => {
+    let user_name = localStorage.getItem("user_name");
+    let user_role = localStorage.getItem("user_role");
+    let user = {};
+    if (user_name && user_role) {
+      user["name"] = user_name;
+      user["role"] = user_role;
+    } else {
+      user["name"] = "Guest";
+      user["role"] = "-1";
+    }
+    console.log(user);
+    setUser(user);
+  };
+
+  const getProfile = (role) => {
+    switch (role) {
+      case "1":
+        navigate("/admin-dashboard");
+        break;
+      case "2":
+        navigate("/user-dashboard");
+        break;
+      case "3":
+        navigate("/BusHome");
+        break;
+      default:
+        console.log("invalid role");
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
   return (
     <Navbar expand="lg" bg="primary" data-bs-theme="dark">
       <Container>
@@ -17,29 +56,45 @@ function BusNavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <div className="ms-auto d-flex align-items-center">
             <PersonCircle className="me-3" color="white"></PersonCircle>
-            <NavDropdown title="Hello Guest" className="text-light fw-bold">
-              <NavDropdown.Item
-                onClick={() => {
-                  navigateTo("/login");
-                }}
+            {user.name === "Guest" ? (
+              <NavDropdown title="Hello Guest" className="text-light fw-bold">
+                <NavDropdown.Item
+                  onClick={() => {
+                    navigateTo("/login");
+                  }}
+                >
+                  Login
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  onClick={() => {
+                    navigateTo("/register-user");
+                  }}
+                >
+                  Register as user
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  onClick={() => {
+                    navigateTo("/register-owner");
+                  }}
+                >
+                  Register as bus owner
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <NavDropdown
+                title={"Hello, " + user.name}
+                className="text-light fw-bold"
               >
-                Login
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                onClick={() => {
-                  navigateTo("/register-user");
-                }}
-              >
-                Register as user
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                onClick={() => {
-                  navigateTo("/register-owner");
-                }}
-              >
-                Register as bus owner
-              </NavDropdown.Item>
-            </NavDropdown>
+                <NavDropdown.Item
+                  onClick={() => {
+                    getProfile(user.role);
+                  }}
+                >
+                  View Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+              </NavDropdown>
+            )}
           </div>
         </Navbar.Collapse>
       </Container>
