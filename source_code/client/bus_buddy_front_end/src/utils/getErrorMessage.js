5fcba3e654b435804358ec08a5af887ab6538a0c
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2023-12-14 18:12:44 +0530

    chore: added error codes

--- source_code/client/bus_buddy_front_end/src/utils/getErrorMessage.js
+++ source_code/client/bus_buddy_front_end/src/utils/getErrorMessage.js
@@ -40,7 +40,19 @@ export function getErrorMessage(errorCode) {
     case "D1015":
     return "Please login using different sign in method";
     case "D1016":
-      return "Payment Intent Creation Failed";      
+      return "Payment Intent Creation Failed";
+    case "D1017":
+      return "Payment Cancelation Failed";
+    case "D1018":
+      return "Start date can't be after end date"
+    case "D1019":
+      return "Responding to complaint failed"
+    case "D1020":
+      return "Cannot Respond to the complaint that is already responded" 
+    case "D1021":
+      return "Complaint with given id doesn't exising";
+    case "D1022":
+      return "Cannot Respond to the complaint as the current user"                 
     case "T0001":
       return "Too many request please try again later";
     default:
