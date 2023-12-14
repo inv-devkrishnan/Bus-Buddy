56d2262ebd1feb3d7806513fdee62e5b917b6122
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2023-12-14 12:03:32 +0530

    feat:added view details in complaint card

--- source_code/client/bus_buddy_front_end/src/components/common/view_complaints/ComplaintCard.jsx
+++ source_code/client/bus_buddy_front_end/src/components/common/view_complaints/ComplaintCard.jsx
@@ -1,8 +1,25 @@
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Card, Form, Modal } from "react-bootstrap";
 import { CheckCircleFill, XCircleFill } from "react-bootstrap-icons";
 
 function ComplaintCard(props) {
+  const [show, setShow] = useState(false);
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  const handleClose = () => {setShow(false); setValidated(false)};
+  const handleShow = () => setShow(true);
   return (
     <Card className="p-3 me-3" style={{ width: "90%" }}>
       <div className="d-flex justify-content-between align-items-top">
@@ -36,8 +53,71 @@ function ComplaintCard(props) {
             </div>
           )}
         </div>
-        <Button style={{ maxHeight: "40px" }}>View Details</Button>
+        <Button
+          style={{ maxHeight: "40px" }}
+          onClick={() => {
+            handleShow();
+          }}
+        >
+          View Details
+        </Button>
       </div>
+      <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>{props.complaint.complaint_title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div style={{ maxHeight: "50vh", overflowY: "scroll" }}>
+            {props.complaint.complaint_body}
+            {props.complaint.status === 0 && (
+              <Form
+                noValidate
+                validated={validated}
+                onSubmit={handleSubmit}
+                className="mt-5 me-2 ms-2"
+              >
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                  <Form.Label className="fw-bold">
+                    Your Response (maximum 5000 characters) :
+                  </Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    required
+                    maxLength={5000}
+                  />
+                    <Form.Control.Feedback type="invalid">
+              required field.
+            </Form.Control.Feedback>
+                </Form.Group>
+                <Button type="submit" variant="success" className="mt-2 mb-5">
+                  Send Response
+                </Button>
+              </Form>
+            )}
+            {props.complaint.status === 1 && (
+              <Form className="mt-5 me-2 ms-2">
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                  <Form.Label className="fw-bold">Your Response:</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    required
+                    maxLength={5000}
+                    value={props.complaint.response}
+                    disabled
+                  />
+                </Form.Group>
+              </Form>
+            )}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Card>
   );
 }
