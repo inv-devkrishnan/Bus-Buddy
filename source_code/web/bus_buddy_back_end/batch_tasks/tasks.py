5f9c8943b02eff479c41a2d68beaf74e594445a4
antony <antony.jose@innovaturelabs.com>
2024-01-04 18:12:42 +0530

    Chore : test case

--- source_code/web/bus_buddy_back_end/batch_tasks/tasks.py
+++ source_code/web/bus_buddy_back_end/batch_tasks/tasks.py
@@ -1,37 +1,33 @@
 from datetime import datetime
 from bus_owner.models import Trip
-import logging
+import logging,pytz
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def UpdateTasksStatus():
     trips= Trip.objects.filter(status=0)
     print(trips)
+    timezone =  pytz.timezone('Asia/Kolkata')
     today =datetime.now()
+    today =timezone.localize(today)
+    print ("new : ",today," timezone : ",today.tzinfo)
     date = today.date()
     time = today.time()
    
     for trip in trips:
-
-        print("inside update task status")
-        print(trip.end_date)
-        print(trip.end_time)
+        print(trip.end_date, "end_date")
+        print(trip.end_time, "end_time")
         print("today")
-        print(time)
-        print(date)
-        print(trip.end_time <= time)
-        print (trip.bus)
-        if (trip.end_date <= date ):
-            print("inside if")
+        
+        print(time,"time")
+        print(date,"date")
+        if (trip.end_date < date ):
             trip.status = 1
             trip.save()
-            logger.info("Task updated to 1")
-        elif trip.end_date == date and trip.end_time <time :
-            print("inside elif")
+            logger.info("Trip status updated to 1")
+        if trip.end_date == date and trip.end_time <time :
             trip.status = 1
             trip.save()
-            logger.info("Task updated to 1")
-        else:
-            print("in else")
+            logger.info("Trip status updated to 1")
+
             
-        print("outside if")
