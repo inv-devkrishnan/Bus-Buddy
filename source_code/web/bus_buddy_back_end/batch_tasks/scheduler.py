6498b0e512afb1ca89916456f2e6a2414a1c9513
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2024-01-08 11:31:28 +0530

    feat: batch operation for changing booking status

--- source_code/web/bus_buddy_back_end/batch_tasks/scheduler.py
+++ source_code/web/bus_buddy_back_end/batch_tasks/scheduler.py
@@ -1,13 +1,17 @@
+import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.apps import apps
 from batch_tasks.tasks import UpdateTasksStatus
+from batch_tasks.tasks import update_booking_status
 import pytz
 def start():
     print("inside scheduler task")
     scheduler = BackgroundScheduler(timezone=pytz.timezone('Asia/Kolkata'))
-    scheduler.add_job(UpdateTasksStatus, 'interval', seconds=30)  # Adjust the time as needed
+    scheduler.add_job(update_booking_status,'interval', seconds=30)
+    scheduler.add_job(UpdateTasksStatus, 'interval', seconds=40)  # Adjust the time as needed
     scheduler.start()
 
 # Run the scheduler when the Django app is ready
 def ready():
-    start()
+      if os.environ.get('RUN_MAIN'): # scheduler only starts in main process
+        start()
--- source_code/web/bus_buddy_back_end/batch_tasks/tasks.py
+++ source_code/web/bus_buddy_back_end/batch_tasks/tasks.py
@@ -1,33 +1,62 @@
+from django.db.models import Q
 from datetime import datetime
 from bus_owner.models import Trip
-import logging,pytz
+from normal_user.models import Bookings
+import logging, pytz
+
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("django")
+indian_timezone = pytz.timezone("Asia/Kolkata")
+
 
 def UpdateTasksStatus():
-    trips= Trip.objects.filter(status=0)
+    trips = Trip.objects.filter(status=0)
     print(trips)
-    timezone =  pytz.timezone('Asia/Kolkata')
-    today =datetime.now()
-    today =timezone.localize(today)
-    print ("new : ",today," timezone : ",today.tzinfo)
+    timezone = indian_timezone
+    today = datetime.now()
+    today = timezone.localize(today)
+    print("new : ", today, " timezone : ", today.tzinfo)
     date = today.date()
     time = today.time()
-   
+
     for trip in trips:
         print(trip.end_date, "end_date")
         print(trip.end_time, "end_time")
         print("today")
-        
-        print(time,"time")
-        print(date,"date")
-        if (trip.end_date < date ):
+
+        print(time, "time")
+        print(date, "date")
+        if trip.end_date < date:
             trip.status = 1
             trip.save()
             logger.info("Trip status updated to 1")
-        if trip.end_date == date and trip.end_time <time :
+        if trip.end_date == date and trip.end_time < time:
             trip.status = 1
             trip.save()
             logger.info("Trip status updated to 1")
 
-            
+
+def update_booking_status():
+    """Function to update Booking status from booked to invalid for completed trips"""
+    try:
+        current_time = datetime.now(
+            tz=indian_timezone
+        )  # setting timezone to asia kolkata
+        completed_trips = []
+        trips = Trip.objects.filter(status=0)
+        for trip in trips:
+            trip_end_time = datetime.combine(
+                date=trip.end_date,
+                time=trip.end_time,
+                tzinfo=indian_timezone,
+            )
+            if trip_end_time < current_time:
+                completed_trips.append(trip)  # gets trips that are done
+        bookings = Bookings.objects.filter(
+            trip__in=completed_trips, status=0
+        )  # gets bookings of thoose trips
+        count = bookings.count()
+        bookings.update(status=1)  # updates booking status of thoose trips
+        logger.info("booking status changed to invalid for " + str(count) + " bookings")
+    except Exception as e:
+        logger.warn("Update Booking status Failed Reason : " + str(e))
