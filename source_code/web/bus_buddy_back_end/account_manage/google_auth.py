b0764b67ea810d4931deb9bb78092920a357b38b
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2024-01-12 17:04:45 +0530

    chore:made change to line 17 of settings.py

--- source_code/web/bus_buddy_back_end/account_manage/google_auth.py
+++ source_code/web/bus_buddy_back_end/account_manage/google_auth.py
@@ -1,8 +1,9 @@
 from google.auth.transport import requests
 from google.auth.exceptions import InvalidValue, MalformedError
 from google.oauth2 import id_token
-from decouple import config
-
+import os
+from dotenv import load_dotenv
+load_dotenv('busbuddy_api.env')
 
 class Google:
     """Google class to fetch the user info and return it"""
@@ -12,7 +13,7 @@ class Google:
         try:
             idinfo = id_token.verify_oauth2_token(auth_token, requests.Request())
 
-            if "accounts.google.com" in idinfo["iss"] and idinfo["aud"] == config(
+            if "accounts.google.com" in idinfo["iss"] and idinfo["aud"] == os.getenv(
                 "CLIENT_ID"
             ):
                 return idinfo
--- source_code/web/bus_buddy_back_end/bus_buddy_back_end/settings.py
+++ source_code/web/bus_buddy_back_end/bus_buddy_back_end/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv('busbuddy_api.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
