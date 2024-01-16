efec395725260d0d9aed7baf2850b80a1d762ad0
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2024-01-16 10:33:15 +0530

    fixed code smell in adminprofile test

--- source_code/client/bus_buddy_front_end/src/test/AdminProfile.test.js
+++ source_code/client/bus_buddy_front_end/src/test/AdminProfile.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import ProfileView from "../components/admin/profile/ProfileView";
 import AdminProfile from "../components/admin/profile/AdminProfile";
 
 describe("admin profile", () => {
