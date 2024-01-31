6e610bf4a470cc9c1ccc60ff405d1b1cce5ecf3d
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2024-01-31 09:22:27 +0530

    fix: provided proper validation message for via location

--- source_code/client/bus_buddy_front_end/src/components/AddRouteCard.jsx
+++ source_code/client/bus_buddy_front_end/src/components/AddRouteCard.jsx
@@ -4,11 +4,11 @@ import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Table from "react-bootstrap/Table";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 import RouteImage from "../assets/route.jpg";
 import AddRouteLocation from "./AddRouteLocation";
-import { Container, Row} from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { axiosApi, openAxiosApi } from "../utils/axiosApi";
 import { showLoadingAlert } from "./common/loading_alert/LoadingAlert";
 
@@ -21,7 +21,7 @@ export default function AddRouteCard() {
   const [show, setShow] = useState(false);
   const [validated, setValidated] = useState(false);
   const navi = useNavigate();
-  
+
   const handleClose = () => {
     setShow(false);
     setLocationAdded(false);
@@ -51,7 +51,7 @@ export default function AddRouteCard() {
 
     // to get previously added  locations if page refresh
     if (localStorage.getItem("stopLocationList") !== null) {
-      console.log(localStorage.getItem("stopLocationList"))
+      console.log(localStorage.getItem("stopLocationList"));
       let stopLocationList = JSON.parse(
         localStorage.getItem("stopLocationList")
       );
@@ -90,10 +90,7 @@ export default function AddRouteCard() {
       };
       console.log(routeData);
       showLoadingAlert("Adding Route");
-      const response = await axiosApi.post(
-        "bus-owner/add-routes/",
-        routeData
-      );
+      const response = await axiosApi.post("bus-owner/add-routes/", routeData);
       Swal.close();
       if (response.status === 200) {
         console.log("Route Added");
@@ -217,7 +214,8 @@ export default function AddRouteCard() {
                     required
                   />
                   <Form.Control.Feedback type="invalid">
-                    Please provide valid Via location
+                    Please provide valid via location (only alphabetic
+                    characters)
                   </Form.Control.Feedback>
                   <Form.Text muted>
                     Via is the location that helps the travellers to identify in
@@ -240,11 +238,9 @@ export default function AddRouteCard() {
                     />
                     <InputGroup.Text>hrs</InputGroup.Text>
                     <Form.Control.Feedback type="invalid">
-                    Please provide valid Duration
-                  </Form.Control.Feedback>
+                      Please provide valid Duration
+                    </Form.Control.Feedback>
                   </InputGroup>
-
-                
                 </Form.Group>
 
                 <Form.Group className="mb-3">
