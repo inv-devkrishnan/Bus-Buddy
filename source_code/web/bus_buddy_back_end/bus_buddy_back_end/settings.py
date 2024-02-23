1063be86f487a30ad0adca1a9fcf8dad70638311
Devkrishnan <devkrishnan.va@innovaturelabs.com>
2024-02-23 09:37:02 +0530

    chore: changed media path

--- source_code/web/bus_buddy_back_end/bus_buddy_back_end/settings.py
+++ source_code/web/bus_buddy_back_end/bus_buddy_back_end/settings.py
@@ -165,9 +165,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-MEDIA_ROOT = "media/"
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.getenv("MEDIA_PATH")
+print("Current Media Directory : "+MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
